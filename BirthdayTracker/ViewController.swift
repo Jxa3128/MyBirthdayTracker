2f7490f6cd2e286ccff2b6a57aef6e6ee147fcc3
Jorge Avila <jorge.avila@mavs.uta.edu>
2020-07-26 22:51:33 -0500

    init

---
+++ BirthdayTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Jorge Juarez on 7/26/20.
+//  Copyright © 2020 Jorge Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
