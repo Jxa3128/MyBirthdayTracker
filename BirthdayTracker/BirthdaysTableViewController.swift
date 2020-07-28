ace25254c33e2c6d257c53a4483e6846f3884d2e
Jorge Avila <jorge.avila@mavs.uta.edu>
2020-07-28 00:58:50 -0500

    connecting the 2 controllers

--- BirthdayTracker/BirthdaysTableViewController.swift
+++ BirthdayTracker/BirthdaysTableViewController.swift
@@ -86,14 +86,17 @@ class BirthdaysTableViewController: UITableViewController, AddBirthdayViewContro
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let navigationController = segue.destination as! UINavigationController
+        let addBirthdayViewController = navigationController.topViewController as! AddBirthdayViewController
+        addBirthdayViewController.delegate = self
     }
-    */
+    
 
 }
